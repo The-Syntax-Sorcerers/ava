8ce72da7c49f11a203339993331fedb2832c4b79
Bunny <89196633+bunnypp@users.noreply.github.com>
2023-08-14 18:44:42 +1000

    Ipss 35 assignment upload page (#8)
    * added assignment upload page, routed to homepage
    
    * Added basic form elements
    
    * Updated routing after merge
    
    * Resolved issues after merge, linked login to home

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -7,14 +7,19 @@ from .models import Country, City, User
 
 bp = Blueprint("bp", __name__)
 
-
 # a simple page that says hello
 @bp.route('/')
+def login(name='login'):
+    return render_template('auth/login.html', name=name)
+
+# a simple page that says hello
+@bp.route('/home', methods=["GET", "POST"])
 def homepage(name='homepage'):
     return render_template('home.html', name=name)
 
-
 # a simple page that says hello
-@bp.route('/hello', methods=["GET", "POST"])
-def hello():
-    return 'hello'
+@bp.route('/upload',  methods=['GET', 'POST'])
+def upload(name='assignment upload'):
+    if request.method == 'POST':
+        return redirect(url_for('homepage'))
+    return render_template('assignment_upload.html', name=name)
