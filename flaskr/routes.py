f2b58988a73fe3c0a5e7047f74e849efd8a1e261
Yash Patil <mailyashpatil333@gmail.com>
2023-08-14 12:54:30 +1000

    Adjusted Import

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,5 +1,3 @@
-import requests
-
 from flask import request, render_template, redirect, url_for, Blueprint
 from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
