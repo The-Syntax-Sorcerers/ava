8061154652fba5f2ed179baef545631e88481ad2
Yash Patil <mailyashpatil333@gmail.com>
2023-08-14 16:08:34 +1000

    Script Change

--- flaskr/script.py
+++ flaskr/script.py
@@ -1,5 +1,8 @@
-from waitress import serve
+# from waitress import serve
 from main import create_app
+#
+# if __name__ == "__main__":
+#     print("Server Running on localhost:5000")
+#     serve(create_app())
 
-if __name__ == "__main__":
-    serve(create_app())
+app = create_app()
