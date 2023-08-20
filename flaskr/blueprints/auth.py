ce96793909eab619fa88e6ea8306f9f1c89599fb
Talym Myler <mylertalym01@gmail.com>
2023-08-20 19:34:33 +1000

    Removed removed form fields from backend

--- flaskr/blueprints/auth.py
+++ flaskr/blueprints/auth.py
@@ -29,7 +29,7 @@ def login():
 
             # Loading User from db for flask_login
             res = supabase_anon.table('Users').select('*').eq('email', form.email.data).execute().data[0]
-            flask_login.login_user(User(res['id'], res['email'], res['name'], res['faculty'], res['uuid']))
+            flask_login.login_user(User(res['email'], res['name']))
 
             flash('Logged in successfully!', 'success')
             print("Logged In!")
@@ -60,8 +60,6 @@ def signup():
             dto = {
                 "email": form.email.data,
                 "name": form.name.data,
-                "faculty": form.faculty.data,
-                "uuid": user.user.id
             }
 
             supabase_sec.table('Users').insert(dto).execute()
@@ -91,4 +89,3 @@ def logout():
     flask_login.logout_user()
     flash('Logged out successfully!', 'success')
     return redirect(url_for('auth.login'))
-
