b96a4d58da6716021f335ba66226928a94d7a6d3
ypatil <yash.patil@student.unimelb.edu.au>
2023-08-27 20:30:28 +1000

    Tidy up imports

--- ava_api/preprocess.py
+++ ava_api/preprocess.py
@@ -1,22 +1,29 @@
 import os
 import json
+import sys
+import json
 import nltk
-from tqdm import tqdm
+import string
+
 import glob
 import numpy as np
+from tqdm import tqdm
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-nltk.download(["punkt", "stopwords","wordnet"])
 from gensim.models import Word2Vec
-import string
-import json
-import sys
-sys.path.insert(0, '..')  # Point to the parent directory of both ava_api and flask
+
 from flaskr.extensions import supabase_sec
+
+
+nltk.download(["punkt", "stopwords", "wordnet"])
+
+sys.path.insert(0, '..')  # Point to the parent directory of both ava_api and flask
+
+
 supabase = supabase_sec
 sys.path.pop(0)
-
 
 
 # I hope this database model structure should be there
@@ -54,11 +61,11 @@ def extract_text_from_files(user_email, file_path):
     # refers to a single document
     return known_texts, unknown_text
 
+
 def build_corpus(data_directory, user_email):
     corpus = {}
     # given that only one txt file is there so just index that file
     problem_file_paths = glob.glob(os.path.join(data_directory, '*'))[0]
-
 
     known_text, unknown_text = extract_text_from_files(user_email, problem_file_paths)
     corpus[0] = {
@@ -67,6 +74,7 @@ def build_corpus(data_directory, user_email):
     }
 
     return corpus
+
 
 def preprocess_text(text):
     """
@@ -99,6 +107,7 @@ def preprocess_text(text):
 
     return tokens
 
+
 def convert_text_to_vector(texts, model, vector_size):
     """
     Convert a list of texts into their corresponding word2vec vectors
@@ -111,40 +120,43 @@ def convert_text_to_vector(texts, model, vector_size):
         if word_count != 0:
             vector /= word_count
         else:
-          vector = np.zeros(vector_size)
+            vector = np.zeros(vector_size)
         vectors.append(vector)
     return vectors
 
+
 def count_punctuations(texts):
-  """
+    """
   Count the frequency of different punctuations in the texts
   """
-  # Define punctuations to count
-  punctuations = set(['.', ',', ';', ':', '!', '?', '-', '(', ')', '\"', '\'', '`', '/'])
+    # Define punctuations to count
+    punctuations = {'.', ',', ';', ':', '!', '?', '-', '(', ')', '\"', '\'', '`', '/'}
 
-  # Initialize dictionary to count punctuations
-  punctuations_count = {p: 0 for p in punctuations}
+    # Initialize dictionary to count punctuations
+    punctuations_count = {p: 0 for p in punctuations}
 
-  # Count punctuations in text_list
-  for text in texts:
-      for char in text:
-          if char in punctuations:
-              punctuations_count[char] += 1
+    # Count punctuations in text_list
+    for text in texts:
+        for char in text:
+            if char in punctuations:
+                punctuations_count[char] += 1
 
-  # Return list of punctuation counts
-  return list(punctuations_count.values())
+    # Return list of punctuation counts
+    return list(punctuations_count.values())
+
 
 def analyze_sentence_lengths(sentences):
-  """
+    """
   Analyze the lengths of sentences
   """
-  sentence_lengths = [len(sentence.split()) for sentence in sentences]
-  average_length = np.mean(sentence_lengths)
-  count_over_avg = np.sum([length > average_length for length in sentence_lengths])
-  count_under_avg = np.sum([length < average_length for length in sentence_lengths])
-  count_avg = len(sentence_lengths) - count_over_avg - count_under_avg
+    sentence_lengths = [len(sentence.split()) for sentence in sentences]
+    average_length = np.mean(sentence_lengths)
+    count_over_avg = np.sum([length > average_length for length in sentence_lengths])
+    count_under_avg = np.sum([length < average_length for length in sentence_lengths])
+    count_avg = len(sentence_lengths) - count_over_avg - count_under_avg
 
-  return [count_over_avg, count_under_avg, count_avg, average_length]
+    return [count_over_avg, count_under_avg, count_avg, average_length]
+
 
 def analyze_words(texts):
     """
@@ -171,63 +183,65 @@ def analyze_words(texts):
 
 
 def calculate_style_vector(texts):
-  """
+    """
   Calculate the style vector of the texts
   """
-  punctuation_vec = count_punctuations(texts)     # Punctuations stylistic features
-  sentence_vec = analyze_sentence_lengths(texts)  # Sentences stylistic features
-  word_vec = analyze_words(texts)                 # Words stylistic features
-  word_count = np.sum([len(text.split()) for text in texts])
+    punctuation_vec = count_punctuations(texts)  # Punctuations stylistic features
+    sentence_vec = analyze_sentence_lengths(texts)  # Sentences stylistic features
+    word_vec = analyze_words(texts)  # Words stylistic features
+    word_count = np.sum([len(text.split()) for text in texts])
 
-  vector = np.concatenate((punctuation_vec, sentence_vec, word_vec))
+    vector = np.concatenate((punctuation_vec, sentence_vec, word_vec))
 
-  return vector / word_count if word_count else vector
+    return vector / word_count if word_count else vector
 
 
 def get_vectors(texts, w2v_model, vector_size):
-  res = []
-  for text in texts:
-    w2v_vec = np.mean(convert_text_to_vector(text, w2v_model, vector_size), axis=0)
-    style_vec = calculate_style_vector(text)
-    res.append(np.concatenate((w2v_vec, style_vec), axis=None))
+    res = []
+    for text in texts:
+        w2v_vec = np.mean(convert_text_to_vector(text, w2v_model, vector_size), axis=0)
+        style_vec = calculate_style_vector(text)
+        res.append(np.concatenate((w2v_vec, style_vec), axis=None))
 
-  return res
+    return res
+
 
 def vectorize_text_data(data, w2v_model, vector_size, user_email):
-  """
+    """
   Build author data from the corpus
   """
-  res = {}
-  for key,val in tqdm(data.items(), total=len(data)):
-    if len(val['unknown']) == 0:
-      continue
+    res = {}
+    for key, val in tqdm(data.items(), total=len(data)):
+        if len(val['unknown']) == 0:
+            continue
 
-    # fetch already present concatenated vec from database
-    vector_result = supabase.table('users').select('concat_vec').eq('user_email', user_email)
-    if not vector_result.data or vector_result.data[0]['concat_vec'] is None:
+        # fetch already present concatenated vec from database
+        vector_result = supabase.table('users').select('concat_vec').eq('user_email', user_email)
+        if not vector_result.data or vector_result.data[0]['concat_vec'] is None:
 
-        concat_vec = get_vectors(val['known'], w2v_model, vector_size)
-        # Store the computed concat_vec into the database for the user
-        json_string = json.dumps(concat_vec)
-        update_response = supabase.table('users').update({
-            'concat_vec': json_string
-        }).eq('user_email', user_email)
-        # error handling
-        if update_response.error:
-            print("Error updating concat_vec for user:", update_response.error)
+            concat_vec = get_vectors(val['known'], w2v_model, vector_size)
+            # Store the computed concat_vec into the database for the user
+            json_string = json.dumps(concat_vec)
+            update_response = supabase.table('users').update({
+                'concat_vec': json_string
+            }).eq('user_email', user_email)
+            # error handling
+            if update_response.error:
+                print("Error updating concat_vec for user:", update_response.error)
 
-        res[key] = {
-            # need to modify this i.e., if concatenated vec present already then no need to calculate again
-            'known': concat_vec,
-            'unknown': get_vectors(val['unknown'], w2v_model, vector_size),
-        }
-    else:
-        res[key] = {
-            'known': json.loads(vector_result.data[0]['concat_vec']),
-            'unknown': get_vectors(val['unknown'], w2v_model, vector_size),
-        }
+            res[key] = {
+                # need to modify this i.e., if concatenated vec present already then no need to calculate again
+                'known': concat_vec,
+                'unknown': get_vectors(val['unknown'], w2v_model, vector_size),
+            }
+        else:
+            res[key] = {
+                'known': json.loads(vector_result.data[0]['concat_vec']),
+                'unknown': get_vectors(val['unknown'], w2v_model, vector_size),
+            }
 
-  return res
+    return res
+
 
 def preprocess_dataset(data_directory, vector_size=300, user_email=""):
     test_corpus = build_corpus(data_directory, user_email)
