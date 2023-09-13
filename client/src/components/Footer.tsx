ddf6887346e4a0fbe44c5a709717a3c5d124a19f
Talym Myler <mylertalym01@gmail.com>
2023-09-13 16:37:57 +1000

    Add footer component to project

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="bg-footer2">
+            <div className="justify-center items-center m-4">
+                <p className="text-center text-white">© The-Syntax-Sorcerers 2023</p>
+            </div>
+        </footer>
+    )
+}
