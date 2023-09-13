001d156b77e55005c05cfdd94c49f9b2432568ab
Talym Myler <mylertalym01@gmail.com>
2023-09-14 03:06:45 +1000

    Add surrounding info and adjustable edit date and contact email

--- client/src/components/PolicyList.tsx
+++ client/src/components/PolicyList.tsx
@@ -1,5 +1,8 @@
 import { Key } from 'react'
 
+// The privacy policy contact email
+const contactEmail = '[email]'   
+
 // The list of privacy policies to reder in [title, paragraph] pairs
 const policies = [
     ['Introduction', 'Welcome to our AI Text Comparison Tool\'s Privacy Policy. This policy explains how we collect, use, disclose, and safeguard your information when you use our AI tool. Please read this policy carefully to understand our practices.'],
@@ -8,7 +11,7 @@ const policies = [
     ['Information Sharing and Disclosure', 'We do not share, sell, or disclose your personal information or the text you provide for comparison to third parties.'],
     ['Data Security', 'We take reasonable precautions to protect the information collected through our AI tool. However, no method of transmission over the internet or electronic storage is completely secure.'],
     ['Changes to this Privacy Policy', 'We may update this Privacy Policy to reflect changes in our practices. We will notify you of any significant changes via email or by placing a prominent notice on our website.'],
-    ['Contact Us', 'If you have questions about this Privacy Policy, please contact us at [email]'],
+    ['Contact Us', `If you have questions about this Privacy Policy, please contact us at ${contactEmail}`],
 ]
 
 // Allows dynamically adding policy paragraphs to the page
@@ -32,6 +35,7 @@ function PolicyElement({policy, rkey}: {policy: string[], rkey: Key}) {
     )
 }
 
+// Creates a numbered list of policies
 export default function PolicyList() {
     return (
         <ul className='space-y-5'>
--- client/src/routePrivacyPolicy/privacyPolicy.tsx
+++ client/src/routePrivacyPolicy/privacyPolicy.tsx
@@ -3,10 +3,16 @@ import PolicyList from "../components/PolicyList";
 // Import Footer from "../components/Footer";
 
 export default function PrivacyPolicy() {
+    const editDate = '14/08/23'
+   
     return (
         <div className="bg-main bg-cover flex flex-col min-h-screen">
             <LoggedInNavbar />
             <main className="container mx-auto flex-grow box-border p-40 min-h-screen">
+                <div className="mb-4 flex flex-col justify-center items-center">
+                    <h1 className='text-3xl font-bold'>Privacy Policy</h1>
+                    <p className='text-lg'>Last updated: {editDate}</p>
+                </div>
                 <PolicyList />
             </main>
             {/* <Footer /> */}
