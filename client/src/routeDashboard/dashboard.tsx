8603a7aee7410539e171a8b3cd4649e952084fc5
Talym Myler <mylertalym01@gmail.com>
2023-09-13 17:07:02 +1000

    Add footer to assignments page, add consistent footer positioning and code spacing

--- client/src/routeDashboard/dashboard.tsx
+++ client/src/routeDashboard/dashboard.tsx
@@ -15,22 +15,20 @@ export default function Dashboard({subjects} : {subjects: [SubjectObj]}) {
     console.log("Rendering Dash with Subjects:", subjects)
 
     return (
-      <>
         <div className="bg-main bg-cover flex flex-col min-h-screen">
-          <LoggedInNavbar />
-          <main className="container mx-auto flex-grow box-border p-40">
-            <div className="container mx-auto px-4">
-              <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {subjects.map((subject: SubjectObj, rkey: Key) => (
-                  <SubjectCard key={subject.id} sub={subject} rkey={rkey} />
-                ))}
-              </div>
-            </div>
-          </main>
-          <Footer />
+            <LoggedInNavbar />
+            <main className="container mx-auto flex-grow box-border p-40 min-h-screen">
+                <div className="container mx-auto px-4">
+                    <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                        {subjects.map((subject: SubjectObj, rkey: Key) => (
+                            <SubjectCard key={subject.id} sub={subject} rkey={rkey} />
+                        ))}
+                    </div>
+                </div>
+            </main>
+            <Footer />
         </div>
-      </>
     );
 }
 
