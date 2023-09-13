82459701fee363d93116b7e1c2987b319419b3e6
Talym Myler <mylertalym01@gmail.com>
2023-09-13 16:53:58 +1000

    Add footer to the dashboard page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-footer2">
+        <footer className="bg-footer">
             <div className="m-4">
                 <p className="text-center">© The-Syntax-Sorcerers 2023</p>
             </div>
--- client/src/routeDashboard/dashboard.tsx
+++ client/src/routeDashboard/dashboard.tsx
@@ -1,5 +1,6 @@
 import LoggedInNavbar from "../components/LoggedInNavbar";
 import SubjectCard from "../components/subjectCard";
+import Footer from '../components/Footer.tsx'
 import { Key } from 'react'
 
 interface SubjectObj {
@@ -14,10 +15,11 @@ export default function Dashboard({subjects} : {subjects: [SubjectObj]}) {
     console.log("Rendering Dash with Subjects:", subjects)
 
     return (
-        <div className="bg-main bg-cover min-h-screen">
+      <>
+        <div className="bg-main bg-cover flex flex-col min-h-screen">
           <LoggedInNavbar />
-          <main className="container mx-auto p-8">
-            <div className="container mx-auto px-4 py-20">
+          <main className="container mx-auto flex-grow box-border p-40">
+            <div className="container mx-auto px-4">
               <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {subjects.map((subject: SubjectObj, rkey: Key) => (
@@ -26,7 +28,9 @@ export default function Dashboard({subjects} : {subjects: [SubjectObj]}) {
               </div>
             </div>
           </main>
+          <Footer />
         </div>
+      </>
     );
 }
 
