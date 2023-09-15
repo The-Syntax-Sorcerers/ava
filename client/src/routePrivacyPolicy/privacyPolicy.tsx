b4589192b84943cb138a3662584f0eac9135d567
Talym Myler <mylertalym01@gmail.com>
2023-09-15 16:01:27 +1000

    Add cards under privacy policies

--- client/src/routePrivacyPolicy/privacyPolicy.tsx
+++ client/src/routePrivacyPolicy/privacyPolicy.tsx
@@ -1,6 +1,6 @@
 import LoggedInNavbar from "../components/LoggedInNavbar";
 import PolicyList from "../components/PolicyList";
-// Import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 export default function PrivacyPolicy() {
     // The most recent edit date of the privacy policy
@@ -9,14 +9,16 @@ export default function PrivacyPolicy() {
     return (
         <div className="bg-main bg-cover flex flex-col min-h-screen">
             <LoggedInNavbar />
-            <main className="container mx-auto flex-grow box-border p-40 min-h-screen">
-                <div className="mb-4 flex flex-col justify-center items-center">
-                    <h1 className='text-3xl font-bold'>Privacy Policy</h1>
-                    <p className='text-lg'>Last updated: {editDate}</p>
+            <main className="container mx-auto flex-grow box-border p-40 pb-10">
+                <div className="mb-4 flex flex-col justify-center items-center bg-card-hover rounded-lg shadow-md p-4">
+                    <div className="mb-4 flex flex-col justify-center items-center">
+                        <h1 className='text-3xl font-bold'>Privacy Policy</h1>
+                        <p className='text-lg'>Last updated: {editDate}</p>
+                    </div>
+                    <PolicyList />
                 </div>
-                <PolicyList />
             </main>
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
