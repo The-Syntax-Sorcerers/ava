4f0f8bf26ddacfa0a67c1c38e9beff9be983abb7
Talym Myler <mylertalym01@gmail.com>
2023-09-21 02:22:15 +1000

    Remove comments

--- client/src/components/LoggedInNavbar.tsx
+++ client/src/components/LoggedInNavbar.tsx
@@ -26,8 +26,6 @@ export default function LoggedInNavbar() {
         <>
             <nav className="fixed top-5 left-0 w-full flex justify-center items-center">
                 <div className="w-[90%] bg-main px-6 py-3 drop-shadow-lg rounded-full border-2 border-button-blue">
-                {/*<div className="w-5/6 bg-slate-200 px-6 py-3 drop-shadow-lg rounded-md">*/}
-                {/*<div className="w-5/6 bg-neutral-200 px-6 py-3 drop-shadow-lg rounded-md">*/}
                 <div className="container mx-auto px-4 flex justify-between items-center">
                     <div className="flex items-center">
                         <img className="w-10 h-10" src={logo} alt="Ava Logo"/>
