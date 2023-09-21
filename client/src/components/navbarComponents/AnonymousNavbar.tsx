e7e689f809f90939106ca3bac5ad900d815b2134
Talym Myler <mylertalym01@gmail.com>
2023-09-22 01:09:04 +1000

    Stlye card borders and add a border to the navbar

--- client/src/components/navbarComponents/AnonymousNavbar.tsx
+++ client/src/components/navbarComponents/AnonymousNavbar.tsx
@@ -29,7 +29,7 @@ export default function AnonymousNavbar({ handleLoginClick, handleSignupClick }:
     return (
         <>
             <nav className="fixed top-5 left-0 w-full flex justify-center items-center">
-                <div className="w-5/6 px-6 py-3 custom-navbar">
+                <div className="w-5/6 px-6 py-3 custom-navbar border-2 border-accent-primary-100">
                     <div className="container mx-auto px-4 flex justify-between items-center">
                         <div className="flex items-center">
                             <img className="w-10 h-10" src={logo} alt="Ava Logo" />
