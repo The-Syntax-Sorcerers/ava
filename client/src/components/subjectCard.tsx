8d24d5e6de81a3ecd7628dae5c757a90498d1e8b
Talym Myler <mylertalym01@gmail.com>
2023-09-21 17:59:06 +1000

    Increase colours in hovered cards

--- client/src/components/subjectCard.tsx
+++ client/src/components/subjectCard.tsx
@@ -4,7 +4,7 @@ import { Key } from "react";
 export default function SubjectCard({ sub, rkey }: { sub: any, rkey: Key }) {
     return (
         <a href={sub.link}>
-        <div key={rkey} className="bg-neutral-50 hover:border-button-light-blue rounded-lg shadow-md p-4 transform hover:scale-105 transition-transform duration-300 cursor-pointer border-2 border-card-border">
+        <div key={rkey} className="custom-subject-cards">
             <h3 className="text-base font-bold mb-1">{sub.name}</h3>
             <h2 className="text-sm font-semibold">{sub.id}</h2>
         </div>
