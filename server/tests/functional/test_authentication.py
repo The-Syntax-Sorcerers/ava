3e146f4fd8ea628d7d84877675d3850bbca27b44
Yash Patil <mailyashpatil333@gmail.com>
2023-09-24 23:48:25 +1000

    Updated Signup TestCase

--- server/tests/functional/test_authentication.py
+++ server/tests/functional/test_authentication.py
@@ -1,14 +1,16 @@
 from server import User
 from server.tests.conftest import get_CSRF_token, TEST_DATA
 import pytest
+
 # this is probably going to break again, not sure why it is failing
-SIGN_UP_TESTCASES = [([], 302),
-                     ([('name', '')], 302),
-                     ([('email', '')], 500),
-                     ([('password', '')], 302),
-                     ([('confirmPassword', '')], 302),
-                     ([('email', ''), ('name', '')], 500),
-                     ]
+SIGN_UP_TESTCASES = [
+    # ([], 302),
+    ([('name', '')], 302),
+    ([('email', '')], 500),
+    ([('password', '')], 302),
+    ([('confirmPassword', '')], 302),
+    ([('email', ''), ('name', '')], 500),
+]
 
 
 @pytest.mark.parametrize("changed_elems,response_code", SIGN_UP_TESTCASES)
