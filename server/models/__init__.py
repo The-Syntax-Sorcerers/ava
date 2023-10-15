68c4768fb9a86659aeb5a5b9f474136b17c19007
Yash Patil <mailyashpatil333@gmail.com>
2023-10-15 22:31:19 +1100

    Cron Job Setup

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -78,7 +78,7 @@ class User(UserMixin):
         if requesting_user.is_admin or requesting_user.id == user_id:
             try:
                 res = supabase_sec.table('User').select('*').eq('id', id).execute().data
-                if ((res['email'] == user_email) and (res['name'] == user_name)):
+                if (res['email'] == user_email) and (res['name'] == user_name):
                     # Send a DELETE request to the Supabase table to delete the user by ID
                     res = supabase_sec.table('User').delete().eq('id', user_id).execute()
 
@@ -176,7 +176,6 @@ class Subject:
 
     @staticmethod
     def create_subject(temp_sub):
-
         try:
             supabase_sec.table('Subject').insert(temp_sub.get_payload_format()).execute()
         except:
@@ -195,12 +194,13 @@ class Subject:
 class Assignment:
 
     def __init__(self, assignment_id, subject_id,
-                 assignment_name, description, due_datetime=None):
+                 assignment_name, description, submission_locked, due_datetime=None):
         self.id = assignment_id
         self.subject_id = subject_id
         self.name = assignment_name
         self.description = description
         self.due_datetime = due_datetime
+        self.submission_locked = submission_locked
         if due_datetime:
             # Separates datetime into date and time
             self.due_datetime = datetime.datetime.strptime(
@@ -216,7 +216,8 @@ class Assignment:
                 + f'subject_id: {self.subject_id}, '
                 + f'due_datetime: {self.due_datetime}, '
                 + f'due_date: {self.due_date}, '
-                + f'due_time: {self.due_time}')
+                + f'due_time: {self.due_time}, '
+                + f'submission_locked: {self.submission_locked}')
 
     # Returns a specific assignment using a given subject_id and assignment_id
     @staticmethod
@@ -225,17 +226,20 @@ class Assignment:
             '*').eq('id', assignment_id).eq('subject_id', subject_id).execute().data
         if res:
             res = res[0]
-            return Assignment(res['id'], res['subject_id'], res['name'], res['description'], res['due_datetime'])
+            return Assignment(res['id'], res['subject_id'], res['name'], res['description'], res['submission_locked'], res['due_datetime'])
         return None
 
     # Returns all assignments using a given subject_id
     @staticmethod
-    def get_all_assignments(subject_id):
-        res = supabase_sec.table('Assignment').select(
-            '*').eq('subject_id', subject_id).execute().data
+    def get_all_assignments(subject_id=None):
+        if subject_id:
+            res = supabase_sec.table('Assignment').select('*').eq('subject_id', subject_id).execute().data
+        else:
+            res = supabase_sec.table('Assignment').select('*').execute().data
         if res:
-            return [Assignment(r['id'], r['subject_id'], r['name'], r['description'], r['due_datetime']) for r in res]
+            return [Assignment(r['id'], r['subject_id'], r['name'], r['description'], r['submission_locked'], r['due_datetime']) for r in res]
         return []
+
 
 
 class Storage:
@@ -244,6 +248,7 @@ class Storage:
         self.initialised = True
         self.supabase_sec = sec
         self.ass_bucket = 'ava-prod-assignments'
+        self.past_ass_bucket = 'ava-prod-past-assignments'
 
     @staticmethod
     def construct_path(subject_id, assignment_id, user_id):
@@ -284,5 +289,65 @@ class Storage:
             if obj['name'] == user_id:
                 return True
         return False
+
+    def cron_get_all_uploaded_assignments(self, subject_id, assignment_id):
+        # if the folder is empty, db returns 1 element in list[0]
+        # as a placeholder
+        final_dict = {}
+        try:
+            res = self.supabase_sec.storage.from_(self.ass_bucket).list(f'{subject_id}/{assignment_id}')
+            for obj in res:
+                temp_user_id = obj['name']
+                final_dict[temp_user_id] = self.download_assignment(subject_id, assignment_id, temp_user_id)
+            self.__cron_delete_entire_assignments_folder(subject_id, assignment_id)
+            self.supabase_sec.table('Assignment').update({'submission_locked': True}).eq('subject_id', subject_id).eq('id', assignment_id).execute()
+        except:
+            pass
+
+        return final_dict
+
+    def __cron_delete_entire_assignments_folder(self, subject_id, assignment_id):
+        try:
+            path = f'{subject_id}/{assignment_id}'
+            return self.supabase_sec.storage.from_(self.ass_bucket).remove(path)
+        except:
+            return None
+
+
+class PastStorage:
+
+    def __init__(self, sec=supabase_sec):
+        self.initialised = True
+        self.supabase_sec = sec
+        self.ass_bucket = 'ava-prod-past-assignments'
+
+    @staticmethod
+    def construct_path(user_id, assignment_id):
+        return f'{user_id}/{assignment_id}'
+
+    def upload_assignment(self, file, user_id, assignment_id):
+        path = self.construct_path(user_id, assignment_id)
+        if not self.exists_assignment(user_id, assignment_id):
+            return self.supabase_sec.storage.from_(self.ass_bucket).upload(path, file)
+        return None
+
+    def download_assignment(self, user_id, assignment_id):
+        # Will return a byte stream.
+        try:
+            path = self.construct_path(user_id, assignment_id)
+            return self.supabase_sec.storage.from_(self.ass_bucket).download(path)
+        except:
+            return None
+
+    def exists_assignment(self, user_id, assignment_id):
+        # if the folder is empty, db returns 1 element in list[0]
+        # as a placeholder
+        res = self.supabase_sec.storage.from_(self.ass_bucket).list(
+            f'{user_id}/{assignment_id}')
+        for obj in res:
+            if obj['name'] == user_id:
+                return [obj]
+        return []
+
 
 
